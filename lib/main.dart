fe90d66a03c6c3c2ad2a005e8e822c0cf2de8908
kekland <kk.erzhan@gmail.com>
2018-11-03 21:56:15 +0600

    Minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -33,14 +33,6 @@ class MyApp extends StatelessWidget {
       title: 'enis',
       theme: new ThemeData(
         brightness: Brightness.dark,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         accentColor: Colors.blue.shade500,
       ),
       builder: (context, child) {
